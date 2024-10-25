56f54c8048ec1c543fbccc5c782eb9e495440736
M16239X <M16239X@cimbniaga.co.id>
2024-10-25 09:42:05 +0700

    initial commit

---
+++ src/main/java/org/example/practiceoauth/PracticeOauthApplication.java
@@ -0,0 +1,13 @@
+package org.example.practiceoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/practiceoauth/PracticeOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.practiceoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
