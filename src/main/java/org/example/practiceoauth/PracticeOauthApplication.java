8468f4e6dffda996b1e23079fd62ddb7829fae03
M16239X <M16239X@cimbniaga.co.id>
2024-10-25 14:06:20 +0700

    feat: oauth2 login

--- src/main/java/org/example/practiceoauth/PracticeOauthApplication.java
+++ src/main/java/org/example/practiceoauth/PracticeOauthApplication.java
@@ -2,8 +2,10 @@ package org.example.practiceoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class PracticeOauthApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/example/practiceoauth/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package org.example.practiceoauth.config;
+
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+}
